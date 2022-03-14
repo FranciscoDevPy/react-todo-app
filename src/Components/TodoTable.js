320a47617a2da60691fa2119e893b5b07006a0dd
FranciscoDevPy <javiier.coolman@gmail.com>
2022-03-13 23:08:08 -0300

    Se agrego el cdn de animate.css Se agrego el efecto fadeIn al botón de eliminar que se encuentra en la tabla de tareas

--- src/Components/TodoTable.js
+++ src/Components/TodoTable.js
@@ -17,16 +17,35 @@ const TodoTable = ( {todo, setTodo, setTodoTotal} ) =>
             });
     }
 
+    const handleShowBtnDelete = (e)=>{
+        const btnDelete = e.currentTarget.children[1].children[0];
+        btnDelete.classList.remove('oculto');
+        btnDelete.classList.add('animate__fadeIn');
+    }
+
+    const handleHideBtnDelete = (e)=>{
+        const btnDelete = e.currentTarget.children[1].children[0];
+        btnDelete.classList.add('oculto');
+        btnDelete.classList.remove('animate__fadeIn');
+    }
+
     return ( 
         <table className="table-todo-app w-100">
             <tbody>
                 { !data?.length ?
                     <tr><td className='text-center' >No todo list</td></tr> :
                     data.map((value)=>(
-                        <tr key={value.id} >
+                        <tr 
+                            key={value.id} 
+                            onMouseOver={(e)=>handleShowBtnDelete(e)}
+                            onMouseLeave={(e)=>handleHideBtnDelete(e)} >
                             <td>{value.todo}</td>
                             <td>
-                                <button className="btn-delete txt-white" onClick={()=>{handleSubmit(value.id)}} >
+                                <button 
+                                    title='Eliminar tarea'
+                                    className="oculto animate__animated btn-delete txt-white" 
+                                    onClick={()=>{handleSubmit(value.id)}} 
+                                >
                                     <i className="fa-solid fa-trash"></i>
                                 </button>
                             </td>
